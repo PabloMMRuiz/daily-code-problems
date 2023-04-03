dd82bc59dedc9b8364fa5e02b4cea4ea78473ed7
Vaporous <pablommruiz04@gmail.com>
2023-04-03 17:53:28 +0200

    finished day 1

---
+++ day1_add_up_to_k.py
@@ -0,0 +1,16 @@
+"""Daily Coding Problem: Problem #1 [Easy]
+Given a list of numbers and a number k, return whether any two numbers from the list add up to k.
+For example, given [10, 15, 3, 7] and k of 17, return true since 10 + 7 is 17.
+Bonus: Can you do this in one pass?
+"""
+
+def addTo(k, intList):
+    rest = set()
+    for i in intList:
+        if i in rest:
+            return True
+        else:
+            rest.add(k-i)
+    return False
+if __name__ == "__main__":
+    print(addTo(17, [10,15,3,1, 2]))
